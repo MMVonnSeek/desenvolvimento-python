9b7b6043e83e90bb1f5843100911be2a872cd9dd
mmvonnseek <maxmullerfitness@gmail.com>
2026-06-10 17:51:05 -0300

    upload

---
+++ 02-variaveis-tipos/06-entrada/main.py
@@ -0,0 +1,10 @@
+# Capturando texto
+nome = input('Digite seu nome: ')
+print(f'Olá,  {nome}')
+
+# Capturando número - COM e SEM conversão
+# SEM conversão (errado para calculo)
+# idade = input('idade')
+# print(idade + 1) # TypeError!
+
+# COM conversão (correto):
